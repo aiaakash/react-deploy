cc7533ca5374060e96707ce96899ea6e9fb5aad0
unknown <aakash.cvd@gmail.com>
2022-11-01 15:48:22 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./home"
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <h1>My App</h1>
       <h2>Author : Aakash</h2>
       <button> Click Here! </button>
+      <Home />
     </div>
   );
 }
---
+++ src/home.jsx
@@ -0,0 +1,11 @@
+// import "./navbar.scss";
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <h2>Test</h2>
+        </div>
+    );
+};
+
+export default Navbar;
