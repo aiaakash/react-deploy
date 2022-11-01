f474b02afa53ee931c8b34295f7ec02aaa39ab0b
unknown <aakash.cvd@gmail.com>
2022-11-01 16:53:12 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from "./home"
 
 function App() {
   return (
     <div className="App">
-      <h1>My App</h1>
-      <h2>Author : Aakash</h2>
-      <button> Click Here! </button>
       <Home />
     </div>
   );
 }
+
 
 export default App;
--- src/home.jsx
+++ src/home.jsx
@@ -1,11 +1,93 @@
-// import "./navbar.scss";
+import styled from "styled-components";
+import { Badge } from '@mui/material';
+// import { Search, ShoppingCartOutlined } from "@mui/material/Icon";
+import SearchIcon from '@mui/icons-material/Search';
+import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
+import React from "react";
 
-const Navbar = () => {
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+
+
+const Home = () => {
     return (
-        <div className="navbar">
-            <h2>Test</h2>
-        </div>
+        <Container>
+            <Wrapper>
+                <Left>
+                    {/* <Language>EN</Language> */}
+                    {/* <SearchContainer>
+                        <Input placeholder="Search" />
+                        <SearchIcon style={{ color: "gray", fontSize: 16 }} />
+                    </SearchContainer> */}
+                </Left>
+                <Center>
+                    <Logo>Aakash Chavda</Logo>
+                </Center>
+                <Right>
+                    <MenuItem>ABOUT</MenuItem>
+                    <MenuItem>RESUME</MenuItem>
+                    <MenuItem>
+                        {/* <Badge badgeContent={4} color="primary">
+                            <LocalGroceryStoreIcon />
+                        </Badge> */}
+                    </MenuItem>
+                </Right>
+            </Wrapper>
+        </Container>
     );
 };
 
-export default Navbar;
+export default Home;
