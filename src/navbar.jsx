fc06100d0d6314863167a559077a47f6c01dff58
unknown <aakash.cvd@gmail.com>
2022-11-05 00:14:32 +0530

    deploy my react app to github pages

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -8,11 +8,11 @@ import { Link } from "react-router-dom";
 
 const Container = styled.div`
   height: 120px;
-  
+  padding: 10px 20px;
 `;
 
 const Wrapper = styled.div`
-  padding: 10px 20px;
+  // padding: 10px 20px;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -77,44 +77,85 @@ const MenuItem = styled.div`
 `;
 
 
+const Mogo = styled.a`
+  display: block;
+  flex: none;
+  float: left;
+  font-size: 1.5em;
+  text-decoration: none;
+  margin-top: 10px;
+  line-height: normal;
+`;
+
+
+const Logoname = styled.span`
+  font-family: "Agustina Regular";
+  font-weight: bold;
+  font-variant-ligatures: no-common-ligatures;
+  -webkit-font-variant-ligatures: no-common-ligatures;
+  padding: 0 10px;
+  color: #f5f2f4
+`;
+
+const Header = styled.header`
+  background-color: $lightBackground1;
+  max-width: 100%;
+  padding: 10px 20px;
+  margin: 0px auto;
+`;
+
+const Greyco = styled.span`
+  color: #868e96;
+`;
+
 const Navbar = () => {
   return (
-    <Container>
-      <Wrapper>
-        {/* <Left> */}
-        {/* <Language>EN</Language> */}
-        {/* <SearchContainer>
+    <>
+
+      <Container>
+        <Header>
+          <Mogo>
+            <Greyco> &lt;</Greyco>
+            <Logoname>Aakash Chavda</Logoname>
+            <Greyco>/&gt;</Greyco>
+          </Mogo>
+        </Header>
+        <Wrapper>
+          {/* <Left> */}
+          {/* <Language>EN</Language> */}
+          {/* <SearchContainer>
                         <Input placeholder="Search" />
                         <SearchIcon style={{ color: "gray", fontSize: 16 }} />
                     </SearchContainer> */}
-        {/* </Left> */}
-        {/* <Center>
+          {/* </Left> */}
+          {/* <Center>
           <Link to="/react-deploy" style={{ textDecoration: "none", color: "#E7F6F2" }} >
             <Logo>Aakash Chavda</Logo>
           </Link>
         </Center> */}
-        {/* <Right> */}
-        <Center>
-          <Link to="/react-deploy/" style={{ textDecoration: "none", color: "#E7F6F2" }}>
-            <MenuItem>HOME</MenuItem>
-          </Link>
-          <Link to="/react-deploy/resume" style={{ textDecoration: "none", color: "#E7F6F2" }}>
-            <MenuItem>RESUME</MenuItem>
-          </Link>
-          {/* <Link to="/react-deploy/resume" style={{ textDecoration: "none", color: "#E7F6F2" }}>
+          {/* <Right> */}
+          <Center>
+            <Link to="/react-deploy/" style={{ textDecoration: "none", color: "#E7F6F2" }}>
+              <MenuItem>HOME</MenuItem>
+            </Link>
+            <Link to="/react-deploy/resume" style={{ textDecoration: "none", color: "#E7F6F2" }}>
+              <MenuItem>RESUME</MenuItem>
+            </Link>
+            {/* <Link to="/react-deploy/resume" style={{ textDecoration: "none", color: "#E7F6F2" }}>
             <MenuItem>SKILLS</MenuItem>
           </Link> */}
-          <Link to="/react-deploy/about" style={{ textDecoration: "none", color: "#E7F6F2" }}>
-            <MenuItem>ABOUT</MenuItem>
-          </Link>
-          <Link to="/react-deploy/contact" style={{ textDecoration: "none", color: "#E7F6F2" }}>
-            <MenuItem>CONTACT</MenuItem>
-          </Link>
-        </Center>
-        {/* </Right> */}
-      </Wrapper>
+            <Link to="/react-deploy/about" style={{ textDecoration: "none", color: "#E7F6F2" }}>
+              <MenuItem>ABOUT</MenuItem>
+            </Link>
+            <Link to="/react-deploy/contact" style={{ textDecoration: "none", color: "#E7F6F2" }}>
+              <MenuItem>CONTACT</MenuItem>
+            </Link>
+          </Center>
+          {/* </Right> */}
+        </Wrapper>
 
-    </Container>
+      </Container>
+    </>
   );
 };
 
